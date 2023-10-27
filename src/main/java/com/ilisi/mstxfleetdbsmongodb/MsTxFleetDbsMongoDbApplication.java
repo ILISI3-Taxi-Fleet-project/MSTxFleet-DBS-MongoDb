6f0d3e167eca0dbc828c8354bcb135dc7b20b7a5
musdev <w0l1d.dev@gmail.com>
2023-10-27 09:37:46 +0100

    init commit

---
+++ src/main/java/com/ilisi/mstxfleetdbsmongodb/MsTxFleetDbsMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetdbsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTxFleetDbsMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTxFleetDbsMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilisi/mstxfleetdbsmongodb/MsTxFleetDbsMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetdbsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetDbsMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
