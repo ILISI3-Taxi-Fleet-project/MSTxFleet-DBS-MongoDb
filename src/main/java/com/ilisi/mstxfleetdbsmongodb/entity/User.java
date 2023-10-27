38b2c313279e7cadfc10b0094df4aca0b2c8abf2
yassine <yassine@gmail.com>
2023-10-27 12:16:47 +0100

    #1 I've added the user management.

---
+++ src/main/java/com/ilisi/mstxfleetdbsmongodb/entity/User.java
@@ -0,0 +1,26 @@
+package com.ilisi.mstxfleetdbsmongodb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private UUID id;
+    private String username;
+    private String password;
+    private String user_type;
+    private String nom;
+    private String prenom;
+    private String email;
+
+}
---
+++ src/main/java/com/ilisi/mstxfleetdbsmongodb/listener/UUIDListener.java
@@ -0,0 +1,20 @@
+package com.ilisi.mstxfleetdbsmongodb.listener;
+
+import com.ilisi.mstxfleetdbsmongodb.entity.User;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class UUIDListener extends AbstractMongoEventListener<User> {
+
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<User> event) {
+        User entity = event.getSource();
+        if (entity.getId() == null) {
+            entity.setId(UUID.randomUUID());
+        }
+    }
+}
