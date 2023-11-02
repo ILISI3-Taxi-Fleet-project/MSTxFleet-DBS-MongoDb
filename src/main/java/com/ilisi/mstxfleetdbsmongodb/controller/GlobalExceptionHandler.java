b38552740928ae909cc501f010bee52b87618473
musdev <w0l1d.dev@gmail.com>
2023-11-02 18:36:32 +0100

    #5 added exception handling for 404 Resource

---
+++ src/main/java/com/ilisi/mstxfleetdbsmongodb/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ilisi.mstxfleetdbsmongodb.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        // Log the exception or return a custom response
+        log.trace("Resource not found", ex);
+//        ex.printStackTrace();
+        log.error("Resource not found", ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+    }
+}
