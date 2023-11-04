2ac20298e322582ee16bee12c7c6d86d155a0663
musdev <w0l1d.dev@gmail.com>
2023-11-04 18:13:19 +0100

    #9 added validation to user

--- src/main/java/com/ilisi/mstxfleetdbsmongodb/entity/User.java
+++ src/main/java/com/ilisi/mstxfleetdbsmongodb/entity/User.java
@@ -1,9 +1,14 @@
 package com.ilisi.mstxfleetdbsmongodb.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
@@ -16,12 +21,31 @@ public class User {
 
     @Id
     private UUID id;
-    private String username;
-    private String password;
-    private String user_type;
-    private String nom;
-    private String prenom;
-    private String email;
-    private String phone;
 
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 255, message = "Username must be between 3 and 15 characters long")
+    @Indexed(unique = true)
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    // Here you can also add @Pattern for regex to enforce strong password policies
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "User type cannot be blank")
+    private String user_type;
+
+    @NotBlank(message = "Last name cannot be blank")
+    private String nom;
+
+    @NotBlank(message = "First name cannot be blank")
+    private String prenom;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Phone cannot be blank")
+    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone number is not valid")
+    private String phone;
 }
